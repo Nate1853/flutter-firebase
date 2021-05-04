6a3f9577fb608160f97f95568dea7f21bbbebf9f
ALTERNATE <alternate@ALTERNATEs-MacBook-Pro.local>
2021-05-04 22:01:20 +0200

    ye

--- brew_crew/ios/Flutter/flutter_export_environment.sh
+++ brew_crew/ios/Flutter/flutter_export_environment.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
+<<<<<<< Updated upstream
 export "FLUTTER_ROOT=C:\src\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\Users\natha\_gitrepo\nate_repo\brew_crew"
 export "FLUTTER_TARGET=lib\main.dart"
@@ -13,3 +14,18 @@ export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
 export "PACKAGE_CONFIG=.packages"
+=======
+export "FLUTTER_ROOT=/Users/alternate/Tools/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/alternate/Projects/flutter-firebase/brew_crew"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/alternate/Projects/flutter-firebase/brew_crew/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/alternate/Projects/flutter-firebase/brew_crew/.dart_tool/package_config.json"
+>>>>>>> Stashed changes
